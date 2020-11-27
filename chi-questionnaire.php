f20bc40ae523d6f1fcfd6da0770a45039ffdcfd4
Merchnas <addmarkovic@gmail.com>
2020-11-27 10:49:04 +0100

    starter theam

---
+++ chi-questionnaire.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Chi Questionnaire
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 0.1
+Author: Richard Markovič
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
